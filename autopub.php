34feee0ea4da3cfd6487c930247fc2928aaac2fe
小松 <sahala_2007@126.com>
2012-10-17 22:06:13 +0800

    修复jQuery加载

--- autopub.php
+++ autopub.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: 自动发布
-Version: 1.9
+Version: 1.9.1
 Plugin URL: http://xiaosong.org/share/auto-pub-plugin-update
 Description: 自动发布文章，修正时区问题，增加相关挂载点，自动删除缓存文件。
 Author: 小松
@@ -23,11 +23,11 @@ function dir_is_empty($dir){
 }
 
 function autopub(){
-  emLoadJQuery();
   echo '<script type="text/javascript">$(function(){$.get("'.DYNAMIC_BLOGURL.'?plugin=autopub");})</script>'."\n";
 }
 
 if (!dir_is_empty(AUTOPUB_CACHE_DIR)) {
+  emLoadJQuery();
   addAction('index_footer', 'autopub');
   addAction('adm_footer', 'autopub');
 }
